4bd21ffbd950685c8373c001c1a1687cb2e70ed3
Mo7amed7afezAmer <104891952+Mo7amed7afezAmer@users.noreply.github.com>
2023-04-29 23:17:39 +0200

    Add files via upload

---
+++ project/static/assets/js/scripts.js
@@ -0,0 +1,12 @@
+(function (window, undefined) {
+'use strict';
+let fileUpload = document.getElementById('file-uploader');
+let previewImg = document.getElementById('preview');
+let button = document.getElementById('send-data')
+fileUpload.addEventListener('change', (e) => {
+    if (e.target.files[0]) previewImg.classList.remove('d-none');
+    previewImg.src = URL.createObjectURL(e.target.files[0]);
+});
+})(window);
+
+
